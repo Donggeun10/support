d4af7389d9fe6cc51fbd6328eae7a48a9d45f136
Lee Donggeun <leedg17@netmarble.com>
2025-03-31 10:55:12 +0900

    modified to add detail information

--- src/main/java/com/example/support/configuration/OpenApi30Config.java
+++ src/main/java/com/example/support/configuration/OpenApi30Config.java
@@ -1,15 +1,30 @@
 package com.example.support.configuration;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Experimental Demo Application", version = "v1"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 @SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic")
 @SecurityScheme(name = "apiKeyAuth", type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.HEADER, paramName = "X-API-KEY")
-public class OpenApi30Config {}
+public class OpenApi30Config {
+    @Value("${spring.application.name: rest-api local}")
+    private String appName;
+
+    @Value("${hostName: localHost}")
+    private String hostName;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new io.swagger.v3.oas.models.info.Info()
+                      .title(String.format("REST API from %s %s", appName, hostName))
+                      .version("1.0")
+                      .description("개발 환경용 API 문서입니다."));
+    }
+}
